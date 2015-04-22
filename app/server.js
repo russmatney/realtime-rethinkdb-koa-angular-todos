c89a92560e8030844c814c981c8bb7f0dc136560
Russell Matney <russell.matney@gmail.com>
2015-04-22 18:57:50 -0400

    basic koa, gulpfile, some deps

---
+++ app/server.js
@@ -0,0 +1,6 @@
+var koa = require('koa');
+
+var app = koa();
+
+app.listen(3000);
+console.log('app listening on 3000');
---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+
+gulp.task('serve', function() {
+  console.log('serving');
+})
+
+gulp.task('default', ['serve']);
