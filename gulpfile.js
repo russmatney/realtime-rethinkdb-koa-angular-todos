8415b952cd75cc1d27716ef5e41362d80d5da344
Russell Matney <russell.matney@gmail.com>
2015-04-22 19:00:06 -0400

    nodemon running with babel-node

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('serve', function() {
-  console.log('serving');
-})
+gulp.task('nodemon', function() {
+  nodemon({
+    script: 'app/server.js',
+    env: { NODE_ENV: 'development' },
+    execMap: {
+      js: 'babel-node'
+    }
+  });
+});
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['nodemon']);
