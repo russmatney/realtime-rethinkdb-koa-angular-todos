d3722931587db06ccf533928108367af598f5f6a
Russell Matney <russell.matney@gmail.com>
2015-04-22 19:25:31 -0400

    bootstrap angular app

--- app/dist/app.js
+++ app/dist/app.js
@@ -1,3 +1,14 @@
 import angular from 'angular';
 
 console.log(angular);
+
+var app = angular.module('app', []);
+
+app.controller('TodosController',
+          ['$scope',
+  function( $scope ) {
+    console.log('todos controller');
+  }]
+)
+
+
